b05645a3ab0c10f9c0f1efb1f2465aec9d709836
Víctor <Vctor@DESKTOP-NRRI26O>
2022-02-01 16:26:33 +0100

    Repositorio de prueba

---
+++ debug/src/debug/PruebaEjercicio.java
@@ -0,0 +1,15 @@
+package debug;	
+import java.util.Scanner;
+	 
+	public class PruebaEjercicio {
+	    public static void main(String[] args) {
+	        Scanner leer=new Scanner(System.in);
+
+	        System.out.println("Escriba una frase");
+	        String frase=leer.nextLine();    
+	        char caracteres[]=frase.toCharArray();
+	        for(int i=0;i<frase.length();i++){
+	            System.out.println(caracteres[i]);
+	        } 
+	    }
+	}
